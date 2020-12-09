f215b186ad08a2ffc062794986757e5f8219785a
Shannon Quinn <shannon.quinn91@gmail.com>
2020-12-09 15:33:37 -0500

    API get request fixed.

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -6,20 +6,29 @@ module.exports = (app) => {
 
     //GET to read the db.json file and return all saved notes as JSON
     app.get('/api/notes', (req, res) => {
-        fs.readFile('../db/db.json', {}, (err) => {
-            if (err) {
-                console.log(err);
-            }
-        })
+        // fs.readFile('../db/db.json', {}, (err) => {
+        //     if (err) {
+        //         console.log(err);
+        //     }
+        // })
+        console.log(db);
+        res.json(db);
+
     });
     
     //POST to receive a new note to save on the request body, add it to the db.json, and then return the new note to the client
     app.post('/api/notes', (req, res) => {
-        const body = req.body;
+        //Collect information from user, store in variable
+        const newNote = {
+            title: req.body.title, 
+            text: req.body.text,
+            id: `${db.length + 1}` //give each note a unique id
+        }
         //Pushes new note to json file
-        db.push(body);
+        db.push(newNote);
         //Return new note to client
-        res.json(body);
+        //console.log(db);
+        res.json(db);
     })
     
     //DELETE to receive a query param containing the id of the note to delete
@@ -27,12 +36,11 @@ module.exports = (app) => {
     app.delete('api/notes/:id', (req, res) => {
         //Receives a query param containing the id of the note to delete
         const deleteNote = req.params.id;
-        
         //Finds id of note entry
         const foundNote = db.find(
-            (char) => foundNote.id === deleteNote
+            (entryID) => foundNote(entryID).id === deleteNote
         );
-        //If note idea does not exist
+        //If note id does not exist
         res.json(foundNote ? foundNote : { error: `Note ${deleteNote} was not found.`});
 
         //Deletes note if it exists
